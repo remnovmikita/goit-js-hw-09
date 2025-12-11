c444dc06f27bc9e42f0529c9cbbd14403aaae39a
Nikita <remnovmikita@gmail.com>
2025-12-11 18:49:12 +0200

    first fix

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -5,18 +5,20 @@ const formData = {
 const email = document.querySelector('[name="email"]');
 const textArea = document.querySelector('[name="message"]');
 const form = document.querySelector('.feedback-form');
+const savedData = JSON.parse(localStorage.getItem("feedback-form-state"));
+if (savedData){
+    email.value = savedData.email || "";
+    textArea.value = savedData.message || "";
+    formData.email = savedData.email || "";
+    formData.message = savedData.message || "";
+}
 
 form.addEventListener("input", even =>{
     formData[even.target.name] = even.target.value.trim();
     localStorage.setItem("feedback-form-state", JSON.stringify(formData));
 })
-const savaData = JSON.parse(localStorage.getItem("feedback-form-state"));
-if (savaData){
-    email.value = savaData.email || "";
-    textArea.value = savaData.message || "";
-    formData.email = savaData.email || "";
-    formData.message = savaData.message || "";
-}
+
+
 form.addEventListener("submit", even => {
     even.preventDefault();
     if (formData.email === "" || formData.message === ""){
